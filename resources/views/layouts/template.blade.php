6a34f669099a81e9054cbe3167fa1456f31eee7a
kevin.alvarez <kevin.alvarez@agrizar.com>
2023-12-18 20:11:38 -0600

    primera subida

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <script src="/ventas/resources/js/functions.js"></script>
+    <link href="/ventas/resources/css/styles.css" rel="stylesheet" />
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
